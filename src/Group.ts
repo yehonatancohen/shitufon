8616fbf2b55f0dab8cfafe748ae8396ae54987f1
yehonatancohen <yoncohenyon@gmail.com>
2024-01-24 00:35:33 +0200

    Building base

--- src/Group.ts
+++ src/Group.ts
@@ -1,11 +1,13 @@
-import { Client, GroupChat, Message, GroupParticipant } from 'whatsapp-web.js';
+import { Client, GroupChat, Message, GroupParticipant, MessageMedia } from 'whatsapp-web.js';
 import { ClientController } from './ClientController';
+import { ClientsManager } from './ClientsManager';
 import { Participant } from './Participant';
+import { sleep } from './Util';
 
 export class Group {
     private groupObj: GroupChat | string;
     private groupId: string;
-    private clients: Client[];
+    private clients: ClientController[];
     private messages: Message[];
     private totalParticipants: Participant[];
     private owner: ClientController;
@@ -28,7 +30,7 @@ export class Group {
             let groupObj = this.groupObj as GroupChat;
             let groupParticipants = await groupObj.participants;
             let participantIds = groupParticipants.map(participant => participant.id._serialized);
-            this.currentParticipants = this.initParticipants(participantIds);
+            this.currentParticipants = await this.initParticipants(participantIds);
         }
     }
 
@@ -43,6 +45,17 @@ export class Group {
         return added;
     }
 
+    private async addParticipants(participant: Participant) {
+        if (this.totalParticipants.includes(participant)) {
+            return;
+        }
+        if (typeof this.groupObj == "string") {
+            return;
+        }
+        await this.groupObj.addParticipants([participant.getId()], {autoSendInviteV4: false});
+        await this.updateCurrentParticipants();
+    }
+
     private getParticipantById(participantId: string) {
         for (const participant of this.totalParticipants) {
             if (participant.getId() == participantId) {
@@ -52,33 +65,83 @@ export class Group {
         return false;
     }
 
-    private initParticipants(participantIds: string[]) {
+    private async initParticipants(participantIds: string[]) {
         let res = [];
         for (const participant of participantIds) {
-            res.push(this.createParticipant(participant));
+            res.push(await this.createParticipant(participant));
         }
         return res;
     }
 
-    private createParticipant(participantId: string) {
+    private async createParticipant(participantId: string) {
         let participantObj = this.getParticipantById(participantId);
         if (participantObj) {
             return participantObj;
         }
         participantObj = new Participant(participantId);
         participantObj.addGroup(this);
-        this.totalParticipants.push(participantObj);
+        await this.updateCurrentParticipants();
         return participantObj;
+    }
+
+    public async setImage(image: string) {
+        if (typeof this.groupObj == "string") {
+            return false;
+        }
+        let created_image = await MessageMedia.fromUrl(image) as MessageMedia;
+        let result = await this.groupObj.setPicture(created_image);
+        if (result == null) {
+            return false;
+        }
+        return true;
+    }
+
+    public async setDescription(description: string) {
+        if (typeof this.groupObj == "string") {
+            return false;
+        }
+        let result = await this.groupObj.setDescription(description);
+        if (result == null) {
+            return false;
+        }
+        return true;
+    }
+
+    private getClientIds(clients: ClientController[]) {
+        let clientIds: string[] = [];
+        for (let client of clients) {
+            clientIds.push(client.getClientId());
+        }
+        return clientIds;
+    }
+
+    private addAdmins(admins: ClientController[]) {
+        if(typeof this.groupObj == "string") {
+            return;
+        }
+        this.groupObj.addParticipants(this.getClientIds(admins), {autoSendInviteV4: false});
+        for (const admin of admins) {
+            if (!this.clients.includes(admin)) {
+                this.clients.push(admin);
+            }
+        }
     }
 
     public async initialize(title: string, totalParticipants: string[], admins: ClientController[], description: string, image: string, adminsOnly: boolean) {
         let result = await this.owner.clientObj.createGroup(title, totalParticipants);
-        this.initParticipants(totalParticipants);
+        await this.initParticipants(totalParticipants);
         if (typeof result == "string"){
             return result; // ERROR
         }
+        await this.updateCurrentParticipants();
         this.groupId = result.gid._serialized;
         this.groupObj = await this.owner.getGroupById(this.groupId);
+        if (image != "") {
+            await this.setImage(image);
+        }
+        if (description != "") {
+            await this.setDescription(description);
+        }
         //this.addParticipantList(result.totalParticipants);
         //this.totalParticipants = formattotalParticipants(totalParticipants);
         this.createdAt = new Date();
