bc1121497eb66082e758dcd3be25bb17c9094f42
yehonatancohen <yoncohenyon@gmail.com>
2024-04-26 20:35:31 +0300

    Fixed warming session

--- src/WarmingSession.ts
+++ src/WarmingSession.ts
@@ -1,15 +1,17 @@
 import { Session } from './Session';
 import { ClientsManager } from './ClientsManager';
-import WAWebJS, { MessageMedia } from 'whatsapp-web.js';
+import WAWebJS, { GroupChat, MessageMedia } from 'whatsapp-web.js';
 import { formatPhoneNumber, idToPhoneNumber} from './Util';
 import { sleep } from './Util';
 import * as fs from 'fs';
+import { group } from 'console';
 
 export class WarmingSession extends Session {
 
     protected mainNumber: string;
     protected groupId: string;
     protected sentences: string[];
+    protected locker: boolean = false;
 
     constructor(cm: ClientsManager, clientIds: string[], mainNumber: string = "") {
         super(cm);
@@ -23,7 +25,7 @@ export class WarmingSession extends Session {
     private loadSentences()
     {
         let data = fs.readFileSync('sentences.txt', 'utf8');
-        let s = data.split("\n");
+        let s = data.split("\n").map((sentence) => sentence.trim());
         return s;
     }
 
@@ -37,8 +39,8 @@ export class WarmingSession extends Session {
 
     private async message_callback(clientId: string, message: WAWebJS.Message, sender_number: string, main_number: string = "", main_client: string = "") {
         let client = this.clients[clientId];
-        const chatType = await message.getChat()
-        if (message.type != WAWebJS.MessageTypes.TEXT || message.body == undefined || message.body == null || chatType.isGroup == true)
+        const chatType = await message.getChat() 
+        if (message.type != WAWebJS.MessageTypes.TEXT || message.body == undefined || message.body == null)
             return;
         let mainClientObj = this.clients[main_client];
         main_number = main_number == "" ? client.get_phone_number() : main_number;
@@ -49,7 +51,10 @@ export class WarmingSession extends Session {
             {
                 case "group":
                     if ((await message.getChat()).isGroup && this.groupId == "")
+                    {
                         this.groupId = (await message.getChat()).id._serialized;
+                        this.clients[clientId].sendGroupMessage(this.groupId, "Group set");
+                    }
                     break;
                 case "up":
                     await mainClientObj.sendMessage(main_number, "Up");
@@ -58,22 +63,49 @@ export class WarmingSession extends Session {
         }
         else
         {
-            let client_turn = Math.floor(Math.random() * this.clientIds.length);
-            if (message.from == this.groupId)
+            if ((message.from == this.groupId && clientId == this.clientIds[0]) || (this.locker && clientId == this.clientIds[1]))
             {
-                await sleep(Math.random() * (180 - 35) + 35);
-                this.clients[this.clientIds[client_turn]].sendGroupMessage(this.groupId, this.sentences[Math.floor(Math.random() * this.sentences.length)]);
+                if (this.locker)
+                {
+                    this.locker = false;
+                }
+                let currentTurn = Math.floor(Math.random() * this.clientIds.length);
+                if (currentTurn == 0)
+                {
+                    this.locker = true;
+                }
+                let sleep_time = Math.random() * (180 - 35) + 35;
+                ClientsManager.logManager.info(`Client ${this.clientIds[currentTurn]} sleeping for ${sleep_time} seconds`);
+                await sleep(sleep_time);
+                this.clients[this.clientIds[currentTurn]].sendGroupMessage(this.groupId, this.sentences[Math.floor(Math.random() * this.sentences.length)]);
+                ClientsManager.logManager.info(`Sent message to ${this.groupId}`);
             }
-            ClientsManager.logManager.info(`Sent message to ${this.groupId}`);
         }
 
     }
 
+    private async all_clients_in_group(group_id: string)
+    {
+        let group = await this.clients[this.clientIds[0]].clientObj.getChatById(group_id) as GroupChat;
+        let participants = await group.participants;
+        let participants_numbers = [];
+        for (let participant of participants)
+        {
+            participants_numbers.push(idToPhoneNumber(participant.id._serialized));
+        }
+        return participants_numbers;
+    }
+
     private async add_message_listener() {
-        let client = this.cm.clients[this.clientIds[0]];
+        let client = this.cm.clients[this.clientIds[0], this.clientIds[1]];
         ClientsManager.logManager.info(`Adding message listener for ${client.getClientId()}`);
-        client.clientObj.on('message', async message =>{
-            await this.message_callback(this.clientIds[0], message, message.from, this.mainNumber);
-        });
+        for (let clientId of this.clientIds)
+        {
+            let client = this.cm.clients[clientId];
+            client.clientObj.on('message', async message => {
+                let author = message.author ? message.author : message.from;
+                await this.message_callback(clientId, message, author, this.mainNumber);
+            });
+        }
     }
 }
